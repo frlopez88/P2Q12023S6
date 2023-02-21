c0fc5ecf4ae1a63c95792f9fc64e82981cca7f0c
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-02-20 20:54:57 -0600

    Primer Commit

---
+++ PrimerosPasos/PrimerosPasos/PrimerosPasos/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrimerosPasos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void saludar(object sender, EventArgs e)
+        {
+
+            string nm = txtNombre.Text;
+            lblResultado.Text = $"Un saludo, {nm}!!!";
+
+        }
+    }
+}
